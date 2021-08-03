a83f43d41246a1761f0d06c232b7e03fe0977bc7
Jhon Sebastian Giraldo Montoya <sebastiangiraldo0917@gmail.com>
2021-08-03 14:27:34 -0500

    Agregar archivos de proyecto.

---
+++ APIPalabras/Controllers/PalabrasController.cs
@@ -0,0 +1,38 @@
+﻿using APIPalabras.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace APIPalabras.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PalabrasController : ControllerBase
+    {
+        [HttpGet("{frase}")]
+        public IEnumerable<Palabras> GetByFrase(string frase)
+        {
+            IEnumerable<string> PalabrasObtenidas = ObtenerListaDePalabras(frase);
+            var resultadoPalabrasObtenidas = PalabrasObtenidas.GroupBy(x => x).Select(group => new {
+                Palabra = group.Key,
+                Cantidad = group.Count()
+            }).OrderByDescending(x => x.Cantidad).FirstOrDefault();
+            return Enumerable.Range(0, 1).Select(index => new Palabras
+            {
+                palabra = resultadoPalabrasObtenidas.Palabra,
+                cantidadRepetida = resultadoPalabrasObtenidas.Cantidad,
+                fraseString = frase
+            })
+            .ToArray();
+        }
+        private static IEnumerable<string> ObtenerListaDePalabras(string frase)
+        {
+            string nuevaFrase = Regex.Replace(frase, @"[^\w\@-]", " ", RegexOptions.None, TimeSpan.FromSeconds(1.5));
+            return nuevaFrase.Split(' ').Where(st => !st.Equals(""));
+        }
+    }
+}
---
+++ APIPalabras/Models/Palabras.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIPalabras.Models
+{
+    public class Palabras
+    {
+        public string palabra { get; set; }
+        public int cantidadRepetida { get; set; }
+        public string fraseString { get; set; }
+
+    }
+}
